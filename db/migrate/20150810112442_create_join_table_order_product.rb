7de68d84e4fc9c7d8835aff251491bd38bd199f9
leonard kakande <kakandeleonard@gmail.com>
2015-08-10 10:17:23 -0700

    rails g migration add businessIdToOrder

--- db/migrate/20150810112442_create_join_table_order_product.rb
+++ db/migrate/20150810112442_create_join_table_order_product.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableOrderProduct < ActiveRecord::Migration
   def change
-  	create_table :order_products do |t|
+    create_table :order_products do |t|
       t.references :order, :null => false
       t.references :product, :null => false
       t.integer    :quantity, :null => false
