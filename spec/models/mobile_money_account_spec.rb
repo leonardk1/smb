52a212bf0d3c738563e296883576705f8f71739f
leonard kakande <kakandeleonard@gmail.com>
2015-08-11 14:40:36 -0700

    factories and specs

--- spec/models/mobile_money_account_spec.rb
+++ spec/models/mobile_money_account_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe MobileMoneyAccount, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  	@mobile_money_account = create(:mobile_money_account)
+  end
+
+  subject { @mobile_money_account }
+  it { should be_valid }
+
+  @attributes_array = ["name", "number", "business_id"]
+  attributes_present_or_not?(@attributes_array)
+
+  describe "invalid mobile mobile money account" do
+    before do
+      @invalid_mobile_money_account = MobileMoneyAccount.create(attributes_for(:invalid_mobile_money_account))
+    end
+
+    it "shoud not save" do
+      expect(@invalid_mobile_money_account).to_not be_valid 
+    end
+
+    specify "error message when no name is specified" do
+      expect(@invalid_mobile_money_account.errors.full_messages).to include("Name can't be blank")
+    end    
+
+    specify "error message when no number is speicified" do
+      expect(@invalid_mobile_money_account.errors.full_messages).to include("Number can't be blank")
+    end
+  end
+
+  describe "uniqueness of account number" do
+  	before do
+      @mobile_money_account = MobileMoneyAccount.create(attributes_for(:mobile_money_account,
+      	number: "+255788767668"))
+      @non_unique_mobile_money_account = MobileMoneyAccount.create(attributes_for(:mobile_money_account,
+      	number: "+255788767668"))
+    end
+
+    it "shoud not save" do
+      expect(@non_unique_mobile_money_account).to_not be_valid 
+    end
+  end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,18 @@ end
 def has_attribute(attribute)
   it { should respond_to(attribute.to_sym) }
 end
+
+def attributes_present_or_not?(attributes)
+  #testing validity
+  attributes.each { |attribute|
+    #ensures that all the the required attributes are present
+    context "when #{attribute} is present" do
+      has_attribute(attribute)
+    end
+
+    #checks for absence of any of the required attributes
+    context "when #{attribute} is not present" do
+     requires_attribute(attribute)
+    end
+  }
+end 
