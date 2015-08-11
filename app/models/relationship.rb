3103e558f8e485c38a59cdd0ebec7de6bdb05a26
leonard kakande <kakandeleonard@gmail.com>
2015-08-11 12:45:52 -0700

    fixing indentation

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,5 @@
 class Relationship < ActiveRecord::Base
-	belongs_to :customer, class_name: "Business"
+  belongs_to :customer, class_name: "Business"
   belongs_to :service_provider, class_name: "Business"
   validates :customer_id, presence: true
   validates :service_provider_id, presence: true
