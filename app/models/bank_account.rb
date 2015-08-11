263b795e73e1082639994f82e61c2c3999a03bb2
leonard kakande <kakandeleonard@gmail.com>
2015-08-11 14:40:12 -0700

    adding more configuration to the models

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,2 +1,8 @@
 class BankAccount < ActiveRecord::Base
+  belongs_to :business
+  validates :name, presence: true
+  validates :number, presence: true, uniqueness: true
+  validates :location, presence: true
+  validates :kind, presence: true
+  validates :business_id, presence: true
 end
--- app/models/mobile_money_account.rb
+++ app/models/mobile_money_account.rb
@@ -1,2 +1,6 @@
 class MobileMoneyAccount < ActiveRecord::Base
+  belongs_to :business
+  validates :number, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :business_id, presence: true
 end
