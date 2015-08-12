40a5d48334322ea40d9fffb567c21f247e7efbf0
leonard kakande <kakandeleonard@gmail.com>
2015-08-12 07:44:24 -0700

    fixing indentation

--- spec/factories/role.rb
+++ spec/factories/role.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :role do
-  	name "Admin"
+    name "Admin"
 
-  	factory :invalid_role do
-  		name nil
-  	end
+    factory :invalid_role do
+      name nil
+    end
   end  
 end
