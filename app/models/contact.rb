e6a5a8a9f896a72128bb1f58f3a783cbaa6b4854
leonard kakande <kakandeleonard@gmail.com>
2015-08-12 06:38:36 -0700

    $ rails generate devise Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         :confirmable
 end
