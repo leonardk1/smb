84da6744d5484676cfde5db16d79657f6a460b10
leonard kakande <kakandeleonard@gmail.com>
2015-08-21 06:12:31 -0700

    the signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path
+    new_contact_session_path
+  end
 end
