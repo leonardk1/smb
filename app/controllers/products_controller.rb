b166c839bd4c89e970bf791ae3e5f1b14c3d1644
Leonard Kakande <kakandeleonard@gmail.com>
2016-04-04 12:20:28 +0300

    ready for demo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,33 @@ class ProductsController < ApplicationController
     end   
   end
 
+  def edit
+    @product = Product.find(params[:id])  
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      if @product.update_attributes(product_params)
+        flash[:success] = "Product, Successfully updated"
+        format.html { redirect_to business_products_path(session[:business_id]) }
+      else
+        flash[:error] = "Sorry, Unable to add the new product because
+          #{@product.errors.full_messages.to_sentence}"
+        format.html { render action: "edit" }
+      end
+    end   
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:success] = "Product(#{@product.name}), Successfully deleted"
+    redirect_to business_products_path(session[:business_id])
+  end
+
+
   private
 
   def product_params
